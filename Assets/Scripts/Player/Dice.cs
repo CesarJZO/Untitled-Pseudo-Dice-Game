ed82f1c3d21905530a42b7f5cf834182c8163f16
CesarJZO <cesarjzo@outlook.com>
2022-07-15 15:16:56 -0500

    Add hexagon dice with discrete rotation

---
+++ Assets/Scripts/Player/Dice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dice : MonoBehaviour
+{
+    public float rotation;
+    public UnityEvent<short> onFaceCollisionEnter;
+    public UnityEvent onFaceCollisionExit;
+    public Face[] faces;
+
+    [SerializeField] private float smoothTime;
+    private float _rotateTimer;
+    private float _velocity;
+    
+    public void DisableAllOtherFaces(short id)
+    {
+        for (var i = 0; i < faces.Length; i++)
+        {
+            if (i == id) break;
+            faces[i].GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    public void EnableFaces()
+    {
+        foreach (var face in faces) 
+            face.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    public void OnRotateRight()
+    {
+        transform.Rotate(0,0, -rotation);
+    }
+    
+    public void OnRotateLeft()
+    {
+        transform.Rotate(0,0, rotation);
+    }
+}
---
+++ Assets/Scripts/Player/Face.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Face : MonoBehaviour
+{
+    public short id;
+    private Dice _dice;
+
+    private void Awake()
+    {
+        _dice = GetComponentInParent<Dice>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        _dice.onFaceCollisionEnter?.Invoke(id);
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        _dice.onFaceCollisionExit?.Invoke();
+    }
+}
